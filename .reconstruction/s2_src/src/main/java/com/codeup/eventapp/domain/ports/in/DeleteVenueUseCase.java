00dd4e9c6ab8cd694fb0e8ac443ab67773ff8166
tonys-dev <santiagor.acarlos@gmail.com>
2025-10-30 10:00:00 -0500

    chore: Remove reconstruction artifacts

--- .reconstruction/s2_src/src/main/java/com/codeup/eventapp/domain/ports/in/DeleteVenueUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.codeup.eventapp.domain.ports.in;
-
-/**
- * Use case for deleting a venue (soft delete).
- */
-public interface DeleteVenueUseCase {
-    void execute(Long id);
-}
--- .reconstruction/s2_src/src/main/java/com/codeup/eventapp/infrastructure/web/dto/event/EventResponse.java
+++
@@ -1,7 +0,0 @@
-package com.codeup.eventapp.infrastructure.web.dto.event;
-
-import java.time.LocalDateTime;
-
-import com.codeup.eventapp.infrastructure.web.dto.venue.VenueResponse;
-
-public record EventResponse(Long id, String name, String location, LocalDateTime date, String description, VenueResponse venue) {}
